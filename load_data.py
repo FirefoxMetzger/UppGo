7dfc5aa2cc0a71d4f0bb646744ce86df16e6b128
FirefoxMetzger <sebastian@wallkoetter.net>
2018-04-03 07:29:52 +0200

    learning proceeds quickly, but validation error is high

--- load_data.py
+++ load_data.py
@@ -2,7 +2,8 @@ import sgf
 import glob
 import numpy as np
 from go import Go
-from shutil import copy
+from shutil import copy, rmtree
+import os
 
 from keras.utils import Sequence
 
@@ -25,50 +26,78 @@ def calculate_split_data(data, test_percent, validation_percent):
 
     return training, validation, test
 
-def generate_split(location, test_percent=0.05, validation_percent=0.05):
+def generate_split(location, test_percent=0.05, validation_percent=0.05, delete_existing=True):
     replays = glob.glob(location)
-    training, validation, test = calculate_split_data(replays, test_percent, validation_percent)
+    data_split = calculate_split_data(replays, test_percent, validation_percent)
 
-    for replay in training:
-            copy(replay, "replays/training_set/")
-    for replay in validation:
-            copy(replay, "replays/validation_set/")
-    for replay in test:
-            copy(replay, "replays/test_set/")
+    dataset_locations = [
+        "replays/training_set/",
+        "replays/validation_set/",
+        "replays/test_set/"
+    ]
+
+    for idx in range(3):
+        save_path = dataset_locations[idx]
+        rmtree(save_path)
+        os.mkdir(save_path)
+        for replay in data_split[idx]:
+                copy(replay, save_path)
+
+def check_replay_integrity(game):
+    try:
+        game.root.properties["RE"]
+    except KeyError:
+        return False
+    if game.rest is None:
+        return False
+ 
+    return True
+
+def loadSingleDataFile(replay):
+    positions = "abcdefghijklmnopqrs"
+
+    with open(replay, "r") as f:
+        collection = sgf.parse(f.read())
+
+    if not collection:
+        print("No games in replay %s" % replay)
+        return list()
+
+    game_data = list()
+    for game in collection:
+        if not check_replay_integrity(game):
+            print("Corrupt Replay at %s" % replay)
+            continue
+
+        sim = Go()
+        sim.reset(game.root)
+        for move in game.rest:
+            try:
+                stone_position = move.properties["B"][0]
+            except KeyError:
+                stone_position = move.properties["W"][0]
+            
+            if stone_position == "":
+                action = 361
+            else:
+                x = positions.index(stone_position[0])
+                y = positions.index(stone_position[1])
+                action = np.ravel_multi_index((y,x),(19,19))
+            
+            sim.step(action)
+        game_data.append(sim)
+
+    return game_data
 
 def loadData(location):
     replays = glob.glob(location)
-    positions = "abcdefghijklmnopqrs"
 
     game_data = list()
     for replay in replays:
-        with open(replay, "r") as f:
-            collection = sgf.parse(f.read())
+        replay_data = loadSingleDataFile(replay)
+        for game in replay_data:
+            game_data.append(game)
 
-        for game in collection:
-            sim = Go()
-            try:
-                game.root.properties["RE"]
-            except KeyError:
-                print("Can't find result for %s " %replay)
-                continue
-            sim.reset(game.root)
-            for move in game.rest:
-                try:
-                    stone_position = move.properties["B"][0]
-                except KeyError:
-                    stone_position = move.properties["W"][0]
-                
-                if stone_position == "":
-                    action = 361
-                else:
-                    x = positions.index(stone_position[0])
-                    y = positions.index(stone_position[1])
-                    action = np.ravel_multi_index((y,x),(19,19))
-                
-                sim.step(action)
-
-            game_data.append(sim)
     return game_data
 
 class SupervisedGoBatches(Sequence):
@@ -89,6 +118,7 @@ class SupervisedGoBatches(Sequence):
         
         #smallest prime bigger then total_examples
         modulus_idx = np.argmax(primes > self.total_examples)
+        print("Entering %d residue class ring." % modulus_idx)
         self.modulus = primes[modulus_idx]
 
         self.step_size = np.random.random_integers(0,self.modulus)
@@ -168,6 +198,27 @@ if __name__ == "__main__":
 
     generator = SupervisedGoBatches(training_data, 512)
     print("There are %d training batches" % len(generator))
+
+    prev_state_batch, prev_labels_batch = generator[0]
+    for state_batch, labels_batch in generator:
+        same = False
+        for state, prev_state in zip(state_batch, prev_state_batch):
+            if np.all(state == prev_state):
+                same = True
+                break
+        else:
+            for label, prev_label in zip(labels_batch, prev_labels_batch):
+                print(label["policy"])
+                if np.all(label["policy"] == prev_label["policy"]):
+                    same = True
+                    break
+                if np.all(label["value"] == prev_label["value"]):
+                    same = True
+                    break
+        prev_labels_batch = labels_batch
+        prev_state_batch = state_batch
+        if same:
+            print("Same Batch Found Twice")
 
     generator = SupervisedGoBatches(validation_data, 512)
     print("There are %d validation batches" % len(generator))
